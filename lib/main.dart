19d83bd8dc7713d48a886116b9bc9803be469043
Administrator <wangchao_198912@126.com>
2019-05-20 14:44:36 +0800

    添加收藏交互

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
   Widget build(BuildContext context) {
 //    final wordPair = WordPair.random();
 //    return Text(wordPair.asPascalCase);
@@ -67,11 +68,25 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
-      )
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red: null,
+      ),
+      onTap: () {
+        setState(() {
+         if (alreadySaved) {
+           _saved.remove(pair);
+         } else {
+           _saved.add(pair);
+         }
+        });
+      },
     );
   }
 }
